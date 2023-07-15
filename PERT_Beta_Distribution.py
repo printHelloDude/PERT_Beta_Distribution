7d18fab7a179a642eaae1dc5ccca15e0dab6a2b2
Bogdan <50268998+printHelloDude@users.noreply.github.com>
2023-07-15 16:00:56 +0300

    Update PERT_Beta_Distribution.py
    v0.1.1-alpha code

--- PERT_Beta_Distribution.py
+++ PERT_Beta_Distribution.py
@@ -1,67 +1,70 @@
 import math
 
-tasks = {}  # Dictionary to store tasks
-total_duration = 0  # Total task duration
-sum_variances = 0  # Sum of task deviations
-
 while True:
-    task_title = input("Enter the task title (or 'q' to quit): ")
-    if task_title == 'q':
-        break
-
-    tasks[len(tasks) + 1] = {'title': task_title}
-
-    analysis_type = input("Enter the analysis type (three or two point): ")
-    if analysis_type not in ['three', 'two']:
-        print("Invalid analysis type. Please try again.")
-        continue
+    tasks = {}  # Dictionary to store tasks
+    total_duration = 0  # Total task duration
+    sum_variances = 0  # Sum of task deviations
 
     while True:
-        try:
-            optimistic = int(input("Enter the optimistic estimate in minutes (O): "))
-            pessimistic = int(input("Enter the pessimistic estimate in minutes (P): "))
-
-            if pessimistic < optimistic:
-                print("Error: Pessimistic estimate cannot be less than the optimistic estimate.")
-                continue
-
-            if analysis_type == 'three':
-                most_likely = int(input("Enter the most likely estimate in minutes (M): "))
-
+        task_title = input("Enter the task title (or 'q' to quit): ")
+        if task_title == 'q':
             break
-        except ValueError:
-            print("Invalid input. Please enter integer values and try again.")
 
-    if analysis_type == 'three':
-        duration = round((optimistic + 4 * most_likely + pessimistic) / 6)
+        tasks[len(tasks) + 1] = {'title': task_title}
+
+        analysis_type = input("Enter the analysis type (three or two): ")
+        if analysis_type not in ['three', 'two']:
+            print("Invalid analysis type. Please try again.")
+            continue
+
+        while True:
+            try:
+                optimistic = float(input("Enter the optimistic estimate in hours (O): "))
+                pessimistic = float(input("Enter the pessimistic estimate in hours (P): "))
+
+                if pessimistic < optimistic:
+                    print("Error: Pessimistic estimate cannot be less than the optimistic estimate.")
+                    continue
+
+                if analysis_type == 'three':
+                    most_likely = float(input("Enter the most likely estimate in hours (M): "))
+
+                break
+            except ValueError:
+                print("Invalid input. Please enter numeric values and try again.")
+
+        if analysis_type == 'three':
+            duration = (optimistic + 4 * most_likely + pessimistic) / 6
+        else:
+            duration = (3 * optimistic + 2 * pessimistic) / 5
+
+        tasks[len(tasks)]['duration'] = duration
+        tasks[len(tasks)]['deviation'] = (pessimistic - optimistic) / 6
+
+        # Update total duration and sum of variances
+        total_duration += duration
+        sum_variances += tasks[len(tasks)]['deviation']
+
+    # Calculate project time
+    project_time = total_duration + 2 * math.sqrt(sum_variances ** 2)
+
+    # Display task tab
+    print("\nTask Sheet:")
+    print("------------")
+    for task_num, task in tasks.items():
+        print(f"Task {task_num}: {task['title']} - Duration: {task['duration']:.2f} hours, Deviation: +/- {task['deviation']:.2f} hours")
+
+    # Display project time
+    print(f"\nProject Time: {project_time:.2f} hours")
+
+    # Ask if user wants to quit or reinitialize
+    quit_prompt = input("\nDo you want to quit or restart? (q/r): ")
+    if quit_prompt.lower() == "q":
+        print("Goodbye!")
+        break
+    elif quit_prompt.lower() == "r":
+        print("Restarting...")
+        continue
     else:
-        duration = round((3 * optimistic + 2 * pessimistic) / 5)
-
-    tasks[len(tasks)]['duration'] = duration
-    tasks[len(tasks)]['deviation'] = round((pessimistic - optimistic) / 6, 1)
-
-    # Update total duration and sum of variances
-    total_duration += duration
-    sum_variances += tasks[len(tasks)]['deviation']
-
-# Square the sum of variances
-sum_variances_squared = sum_variances ** 2
-
-# Calculate project time
-project_time = total_duration + 2 * math.sqrt(sum_variances_squared)
-
-# Display task sheet
-print("\nTask Sheet:")
-print("------------")
-for task_num, task in tasks.items():
-    print(f"Task {task_num}: {task['title']} - Duration: {task['duration']} minutes, Deviation: +/- {task['deviation']:.1f} minutes")
-
-# Display project time
-print(f"\nProject Time: {project_time} minutes")
-
-# Ask if user wants to quit
-quit_prompt = input("\nDo you want to quit? (yes/no): ")
-if quit_prompt.lower() == "yes":
-    print("Goodbye!")
-else:
-    print("Continuing...")
+        print("Invalid input. You need to force close the program.")
+        break
